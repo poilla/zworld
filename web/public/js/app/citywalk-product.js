35b33bc395481bcde357c5914642b8cb375d95a2
poilla <1311539951@qq.com>
2016-11-11 19:06:29 +0800

    updata zworld

--- web/public/js/app/citywalk-product.js
+++ web/public/js/app/citywalk-product.js
@@ -10,8 +10,8 @@ define(['jquery', 'app/myfn'], function ($, url) {
 				var proInfo = $('<div class="pro-info"></div>');
 				var proPlace = $('<span class="pro-place">'+elem.address+'</span>')
 				var browerinfo = $('<div class="browerinfo"></div>');
-				var browernum = $('<span class="browernum num">'+elem.browseCount+'</span>次浏览');
-				var buynum = $('<span class="buynum num">'+elem.soldCount+'</span>件已售');
+				var browernum = $('<span class="browernum num">'+elem.browseCount+'</span>');
+				var buynum = $('<span class="buynum num">'+elem.soldCount+'</span>');
 				var proTitle = $('<h2 class="pro-title"><a href="#">'+elem.title+'</a></h2>');
 				var proInfoList = $('<div class="pro-infolist"></div>');
 				elem.introduce.forEach(function (elem1, index1) {
@@ -19,12 +19,12 @@ define(['jquery', 'app/myfn'], function ($, url) {
 					proInfoList.append(proInfoItem);
 				});
 				var proPrice = $('<div class="pro-price"></div>');
-				var oldPrice = $('<span class="oldprice">'+elem.oldPrice+'元</span>');
-				var newPrice = $('<span class="newprice">'+elem.newPrice+'</span> 元起');
+				var oldPrice = $('<span class="oldprice">'+elem.oldPrice+'元 </span> ');
+				var newPrice = $('<span class="newprice">'+elem.newPrice+'</span> ');
 				var timeBuy = $('<div class="timebuy"><a href="#" class="buy">立即订购</a></div>');
 				
-				proPrice.append(oldPrice).append(newPrice);
-				browerinfo.append(browernum).append(buynum);
+				proPrice.append(oldPrice).append(newPrice).append(' 元起');
+				browerinfo.append(browernum).append('次浏览').append(buynum).append('件已售');
 				proInfo.append(proPlace).append(browerinfo);
 				proDesc.append(proInfo).append(proTitle).append(proInfoList).append(proPrice).append(timeBuy);
 				proLink.append(pic);
